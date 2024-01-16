e3f9eebe3314d152d840eb4155bb2ed21511da83
XinCm <lxincm@gmail.com>
2024-01-16 13:37:55 +0800

    Site updated: 2024-01-16 13:37:54

--- js/main.js
+++ js/main.js
@@ -22,8 +22,7 @@
 
     if (typeof moment === 'function') {
         $('.article-meta time').each(function() {
-            /*$(this).text(moment($(this).attr('datetime')).fromNow());*/
-            $(this).text(article0moment($(this).attr('datetime')).format("YYYY-MM-DD"));
+            $(this).text(moment($(this).attr('datetime')).fromNow());
         });
     }
 
